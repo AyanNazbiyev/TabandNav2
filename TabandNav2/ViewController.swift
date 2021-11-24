256a4e0f50d9252bfbaa21645a517649f0beb2a8
Ayan <nazbievayan16@gmail.com>
2021-11-24 22:57:58 +0600

    Initial Commit

---
+++ TabandNav2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabandNav2
+//
+//  Created by Ayan on 24.11.2021.
+//  Copyright © 2021 Ayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
